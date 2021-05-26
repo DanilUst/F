ded5756af6545978008624f7ff286cce79351871
DanilUst <78728660+DanilUst@users.noreply.github.com>
2021-05-26 14:17:15 +0300

    Update tictac.cpp

--- app/tictac.cpp
+++ app/tictac.cpp
@@ -3,22 +3,17 @@
 
 int main()
 {
-	int winner = FALSE;		// For winner: FALSE = 0, X = 1, O = 2
-
-	
-	while (!winner)
-	{
-		DrawBoard();
+	//int winner = FALSE;		// For winner: FALSE = 0, X = 1, O = 	
+	//while (!winner)
+	//{
+	//	DrawBoard();
     		//	GetInput();
-		winner = CheckWinner();
-
-		if (player != O) player = O; else player = X;	// Switches player each turn
-	}
-	DrawBoard();
-	if (winner == DRAW) 
-		std::cout << "DRAW!\n";
-	else 
-		std::cout << "Congratulations Player " << winner << "!\nYou win!\n";	
-
-	
+	//	winner = CheckWinner();
+	//	if (player != O) player = O; else player = X;	// Switches player each turn
+	//}
+	//DrawBoard();
+	//if (winner == DRAW) 
+	//	std::cout << "DRAW!\n";
+	//else 
+	//	std::cout << "Congratulations Player " << winner << "!\nYou win!\n";		
 }
